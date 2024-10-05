0d5a2e365a148d23bb5b9d28dabfdf45c69eadcd
Dr-Smth <smith.dr@me.com>
2024-10-05 22:33:30 +0100

    quit option added to start screen

--- start.py
+++ start.py
@@ -15,18 +15,24 @@ def handle_start_state(screen, font, events, high_score):
     title_text = v_large_font.render("'Sploding Asteroids !!!", True, (255, 140, 0))
     instruction_text = large_font.render("Press ENTER to Start", True, (192, 192, 192))
     select_ship_text = font.render("Press S to Select Your Ship", True, (192, 192, 192))
+    reset_save_text = font.render("Press R to reset Save File", True, (192, 192, 192))
+    quit_text = font.render("Press Q to Quit Game", True, (192, 192, 192))
     high_score_text = medium_font.render(f"High Score: {high_score}", True, (255, 140, 0))
 
     # Get rectangles for positioning
     title_rect = title_text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 200))
     instruction_rect = instruction_text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
     select_ship_rect = select_ship_text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 100))
-    high_score_rect = high_score_text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 250))
+    reset_save_text_rect = reset_save_text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 150))
+    quit_text_rect = quit_text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 200))
+    high_score_rect = high_score_text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 300))
 
     # Blit the text onto the screen
     screen.blit(title_text, title_rect)
     screen.blit(instruction_text, instruction_rect)
     screen.blit(select_ship_text, select_ship_rect)
+    screen.blit(reset_save_text, reset_save_text_rect)
+    screen.blit(quit_text, quit_text_rect)
     screen.blit(high_score_text, high_score_rect)
 
     # Handle events on start screen
@@ -41,5 +47,12 @@ def handle_start_state(screen, font, events, high_score):
             elif event.key == pygame.K_s:
                 # Go to ship selection screen
                 return "ship_select"
+            elif event.key == pygame.K_r:
+                #reset save file
+                pass
+            elif event.key == pygame.K_q:
+                # Quit the game
+                pygame.quit()
+                sys.exit()
 
     return "start"
